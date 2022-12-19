5a867704c3149251f42b6f9e717fce46dd4eeea9
Lucas do Couto Lemos <lemos120402@gmail.com>
2022-12-19 16:51:30 -0300

    deploy settings

--- build.sh
+++ build.sh
@@ -5,9 +5,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
